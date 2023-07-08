550c2ec8d42d7aa6110ec033271b540bc4fc3306
Abdullah <abdullah@chronogames.net>
2023-07-09 00:26:16 +0200

    added falling sound

--- Assets/Scripts/Misc/Respawn.cs
+++ Assets/Scripts/Misc/Respawn.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class Respawn : MonoBehaviour {
 
     public Animator screenDimmer;
+    public AudioSource deathSound;
     private void OnTriggerEnter (Collider other) {
         if (other.tag == "DeathZone") {
             screenDimmer.SetBool ("HasDied", true);
             StartCoroutine(RespawnTimer());
+            deathSound.Play();
         }
     }
 
     private IEnumerator RespawnTimer () {
         yield return new WaitForSeconds (1);
         Scene scene = SceneManager.GetActiveScene();
+        deathSound.Stop();
         SceneManager.LoadScene(scene.name);
     }
 
