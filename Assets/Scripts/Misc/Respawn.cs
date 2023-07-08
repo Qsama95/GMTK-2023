47b8ff89cd4f72124ce237ab45e69021bce2ba66
Abdullah <abdullah@chronogames.net>
2023-07-08 22:36:12 +0200

    fixed respawn

--- Assets/Scripts/Misc/Respawn.cs
+++ Assets/Scripts/Misc/Respawn.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Respawn : MonoBehaviour {
 
     public Animator screenDimmer;
-    public GameObject roomSpawnPoint;
     private void OnTriggerEnter (Collider other) {
         if (other.tag == "DeathZone") {
             screenDimmer.SetBool ("HasDied", true);
@@ -15,8 +15,8 @@ public class Respawn : MonoBehaviour {
 
     private IEnumerator RespawnTimer () {
         yield return new WaitForSeconds (1);
-        gameObject.transform.position = roomSpawnPoint.transform.position;
-        screenDimmer.SetBool ("HasDied", false);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 
 }
